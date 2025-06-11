2c5e628e3c1a5247e46927797c6cfd8619aa20f4
Cuongcuch <nmanhcuong.cpf@gmail.com>
2025-06-11 12:11:37 +0700

    Level 1, 1.2: Write a program that takes a string as input and displays the length of the string.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,19 +6,13 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter first number: ");
-        double num1 = scanner.nextDouble();
+        System.out.print("Enter a string: ");
 
-        System.out.print("Enter second number: ");
-        double num2 = scanner.nextDouble();
+        String userInput = scanner.nextLine();
 
-        // Calculate sum
-        double sum = num1 + num2;
+        int length = userInput.length();
+        System.out.println("The length of the string \"" + userInput + "\" is: " + length);
 
-        // Display result
-        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
-
-        // Close scanner
         scanner.close();
     }
 }
