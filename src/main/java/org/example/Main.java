b4188c34eb382ce8fc7dca4ae9cd02ac54a59a35
Cuongcuch <nmanhcuong.cpf@gmail.com>
2025-06-11 14:16:39 +0700

    Level 1, 1.7: Write a program that takes a list of strings as input and sorts the list in alphabetical order

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,8 +7,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("How many numbers do you want to enter? ");
+
+        System.out.print("How many strings do you want to enter? ");
         int count = scanner.nextInt();
+        scanner.nextLine();
+
 
         if (count <= 0) {
             System.out.println("Please enter a positive number!");
@@ -16,23 +19,21 @@ public class Main {
             return;
         }
 
-        double[] numbers = new double[count];
+        String[] words = new String[count];
 
         for (int i = 0; i < count; i++) {
-            System.out.print("Enter number " + (i + 1) + ": ");
-            numbers[i] = scanner.nextDouble();
+            System.out.print("Enter string " + (i + 1) + ": ");
+            words[i] = scanner.nextLine();
         }
 
 
-        System.out.println("\nOriginal list: " + Arrays.toString(numbers));
+        System.out.println("\nOriginal list: " + Arrays.toString(words));
+
+        Arrays.sort(words, String.CASE_INSENSITIVE_ORDER);
+
+        System.out.println("Sorted: " + Arrays.toString(words));
 
 
-        Arrays.sort(numbers);
-
-
-        System.out.println("Sorted: " + Arrays.toString(numbers));
-
-        // Close scanner
         scanner.close();
     }
 }
