81a5f55cc8801e696338ab95596b8dc4f8c1a744
Cuongcuch <nmanhcuong.cpf@gmail.com>
2025-06-11 15:02:39 +0700

    Level 2, 2.4: Write a program that takes a list of numbers as input and returns the sum of the numbers that are divisible by both 3 and 5.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,31 +4,26 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-    public static String longestCommonSuffix(String s1, String s2) {
-        int len1 = s1.length();
-        int len2 = s2.length();
-        int i = len1 - 1;
-        int j = len2 - 1;
-        StringBuilder result = new StringBuilder();
-
-
-        while (i >= 0 && j >= 0 && s1.charAt(i) == s2.charAt(j)) {
-            result.insert(0, s1.charAt(i));
-            i--;
-            j--;
-        }
-
-        return result.toString();
-    }
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("String 1: ");
-        String s1 = scanner.nextLine();
-        System.out.print("String 2: ");
-        String s2 = scanner.nextLine();
 
-        String lcs = longestCommonSuffix(s1, s2);
-        System.out.println("LSC: " + lcs);
+        System.out.print("Enter numbers ");
+        String input = scanner.nextLine();
+
+        String[] parts = input.split("\\s+");
+        int sum = 0;
+
+        for (String part : parts) {
+            try {
+                int num = Integer.parseInt(part);
+                if (num % 3 == 0 && num % 5 == 0) {
+                    sum += num;
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid number: " + part);
+            }
+        }
+
+        System.out.println("Resolve: " + sum);
     }
 }
