845a8cf5779a777a18fab1f89b71de86453c0bc9
Cuongcuch <nmanhcuong.cpf@gmail.com>
2025-06-11 14:28:14 +0700

    Level 1, 1.9: Write a program that takes a string as input and returns the number of words in the string.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,45 +7,28 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter how many numbers: ");
-        int count = scanner.nextInt();
+
+        System.out.println("Enter a string: ");
+        String input = scanner.nextLine();
 
 
-        if (count <= 0) {
-            System.out.println("Please enter a positive number!");
+        input = input.trim();
+
+        if (input.isEmpty()) {
+            System.out.println("The string is empty. Word count: 0");
             scanner.close();
             return;
         }
 
 
-        double[] numbers = new double[count];
+        // regex \\s is a regular expression that matches one or more whitespace characters
+        String[] words = input.split("\\s+");
+        int wordCount = words.length;
 
-        for (int i = 0; i < count; i++) {
-            System.out.print("Enter number " + (i + 1) + ": ");
-            numbers[i] = scanner.nextDouble();
-        }
+        // Display results
+        System.out.println("Number of words: " + wordCount);
 
-        System.out.println("\nOriginal list: " + Arrays.toString(numbers));
-
-
-        Arrays.sort(numbers);
-
-        System.out.println("Sorted: " + Arrays.toString(numbers));
-
-        // Calculate median
-        double median;
-        if (count % 2 == 0) {
-            int middleIndex1 = count / 2 - 1;
-            int middleIndex2 = count / 2;
-            median = (numbers[middleIndex1] + numbers[middleIndex2]) / 2.0;
-        } else {
-            int middleIndex = count / 2;
-            median = numbers[middleIndex];
-        }
-
-        System.out.println("\nThe median: " + median);
-
-
+        // Close scanner
         scanner.close();
     }
 }
