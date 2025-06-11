4d9088dbe8e2d1df8531ef62540913f1be66d184
Cuongcuch <nmanhcuong.cpf@gmail.com>
2025-06-11 16:46:42 +0700

    Level 3, 3.5: Write a program that takes a list of numbers as input and returns the smallest positive integer that cannot be represented as the sum of any subset of the list.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,56 +1,34 @@
 package org.example;
-
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
+    public static int findSmallest(int[] arr) {
+        Arrays.sort(arr);
+        int res = 1;
+//[1 ,1 ,2 ,4,]
+        for (int num : arr) {
+            if (num > res) {
+                break;
+            }
+            res += num;
+        }
+
+        return res;
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Nhập số lượng: ");
-        int n = scanner.nextInt();
 
+        System.out.print("Nhập: ");
+        String[] parts = scanner.nextLine().split("\\s+");
+        int[] numbers = new int[parts.length];
 
-        int[] arr = new int[n];
-        System.out.println("Nhập " + n + " số nguyên:");
-        for (int i = 0; i < n; i++) {
-            System.out.print("Nhập số thứ " + (i + 1) + ": ");
-            arr[i] = scanner.nextInt();
+        for (int i = 0; i < parts.length; i++) {
+            numbers[i] = Integer.parseInt(parts[i]);
         }
 
-
-
-        int[] dp = new int[n];
-        int[] prev = new int[n];
-        Arrays.fill(dp, 1);
-        Arrays.fill(prev, -1);
-
-        int maxLength = 1;
-        int endIndex = 0;
-
-
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (arr[i] > arr[j] && dp[i] < dp[j] + 1) {
-                    dp[i] = dp[j] + 1;
-                    prev[i] = j;
-                    if (dp[i] > maxLength) {
-                        maxLength = dp[i];
-                        endIndex = i;
-                    }
-                }
-            }
-        }
-
-//        ArrayList<Integer> sequence = new ArrayList<>();
-//        for (int i = endIndex; i >= 0; i = prev[i]) {
-//            sequence.add(0, arr[i]);
-//        }
-
-
-        System.out.println("Độ dài của chuỗi con tăng dần dài nhất: " + maxLength);
-//        System.out.println("Một chuỗi con tăng dần dài nhất: " + sequence);
-
-        scanner.close();
+        int result = findSmallest(numbers);
+        System.out.println("result: " + result);
     }
 }
