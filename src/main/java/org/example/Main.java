0ee3cc1897db07d6bac257bfb7883c24e8ba54e7
Cuongcuch <nmanhcuong.cpf@gmail.com>
2025-06-12 11:21:58 +0700

    Level 4, 4.3: Write a program that takes a list of strings as input and returns the length of the longest substring that appears in every string in the list.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,28 +5,47 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Nhập số phần tử: ");
+        System.out.print("Nhập số chuỗi: ");
         int n = scanner.nextInt();
+        scanner.nextLine();
 
-        int[] arr = new int[n];
-        System.out.println("Nhập các phần tử:");
+        String[] strings = new String[n];
         for (int i = 0; i < n; i++) {
-            arr[i] = scanner.nextInt();
+            strings[i] = scanner.nextLine();
         }
 
-        System.out.print("target: ");
-        int target = scanner.nextInt();
-
-        System.out.println("Kết quả: " + demCapSo(arr, target));
+        System.out.println("Kết quả: " + timChuoiChungDaiNhat(strings));
     }
 
-    public static int demCapSo(int[] arr, int target) {
-        int count = 0;
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = i + 1; j < arr.length; j++) {
-                if (arr[i] + arr[j] == target) count++;
+    public static int timChuoiChungDaiNhat(String[] strings) {
+        //tim chuoi ngan nhat
+        String chuoiNgan = strings[0];
+        for (String s : strings) {
+            if (s.length() < chuoiNgan.length()) {
+                chuoiNgan = s;
             }
         }
-        return count;
+
+        // Kiểm tra từng độ dài có thể
+        for (int len = chuoiNgan.length(); len > 0; len--) {
+            for (int i = 0; i <= chuoiNgan.length() - len; i++) {
+                String sub = chuoiNgan.substring(i, i + len);
+                boolean timThay = true;
+
+                // check neu khong co break luon
+                for (String s : strings) {
+                    if (!s.contains(sub)) {
+                        timThay = false;
+                        break;
+                    }
+                }
+
+                if (timThay){
+                    return len;
+                }
+            }
+        }
+
+        return 0;
     }
 }
