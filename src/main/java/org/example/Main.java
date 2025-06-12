027cab63d5533ac8ca3c28c7be869502706bff9f
Cuongcuch <nmanhcuong.cpf@gmail.com>
2025-06-12 09:27:12 +0700

    Level 3, 3.7: Write a program that takes a string as input and returns the length of the longest palindrome that can be formed by rearranging the characters in the string.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,67 +4,44 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        List<String> strings = new ArrayList<>();
+
+        // Nhập chuỗi
+        System.out.println("Nhập chuỗi: ");
+        String input = scanner.nextLine();
 
 
-        System.out.print("Nhập số chuỗi: ");
-        int n = scanner.nextInt();
-        scanner.nextLine();
-
-
-        System.out.println("Nhập các chuỗi:");
-        for (int i = 0; i < n; i++) {
-            System.out.print("Chuỗi " + (i + 1) + ": ");
-            strings.add(scanner.nextLine());
-        }
-
-
-        String[] result = findLargestOverlap(strings);
-
-
-        System.out.println("\nHai chuỗi có nhiều ký tự trùng nhau nhất:");
-        System.out.println(result[0] + ", " +result[1]);
+        int result = findLongestPalindrome(input);
+        System.out.println("palindrome dài nhất: " + result);
 
         scanner.close();
     }
 
-    public static String[] findLargestOverlap(List<String> strings) {
-        String[] result = new String[2];
-        int maxOverlap = -1;
+    public static int findLongestPalindrome(String s) {
+        s = s.toLowerCase().replaceAll(" ", "");
 
+        // Đếm số lần xuất hiện của mỗi ký tự
+        Map<Character, Integer> charCount = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            charCount.put(c, charCount.getOrDefault(c, 0) + 1);
+        }
 
-        for (int i = 0; i < strings.size(); i++) {
-            for (int j = i + 1; j < strings.size(); j++) {
-                String str1 = strings.get(i);
-                String str2 = strings.get(j);
+        int length = 0;
+        boolean hasOdd = false;
 
-
-                int overlap = calculateOverlap(str1, str2);
-
-                if (overlap > maxOverlap) {
-                    maxOverlap = overlap;
-                    result[0] = str1;
-                    result[1] = str2;
-                }
+        // Tính độ dài palindrome
+        for (int count : charCount.values()) {
+            if (count % 2 == 0) {
+                length += count;
+            } else {
+                length += count - 1;
+                hasOdd = true;
             }
         }
 
-        return result;
-    }
-
-    public static int calculateOverlap(String str1, String str2) {
-        Set<Character> set1 = new HashSet<>();
-        for (char c : str1.toCharArray()) {
-            set1.add(c);
-        }
-        Set<Character> set2 = new HashSet<>();
-        for (char c : str2.toCharArray()) {
-            set2.add(c);
+        if (hasOdd) {
+            length += 1;
         }
 
-
-        set1.retainAll(set2);
-
-        return set1.size();
+        return length;
     }
 }
