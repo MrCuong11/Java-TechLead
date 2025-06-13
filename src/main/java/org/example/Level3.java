2086c75b4223b82af1af2c701dba4152e07a4c3f
Cuongcuch <nmanhcuong.cpf@gmail.com>
2025-06-13 10:01:28 +0700

    3.3, 3.5

--- src/main/java/org/example/Level3.java
+++ src/main/java/org/example/Level3.java
@@ -8,6 +8,8 @@ public class Level3 {
         Scanner scanner = new Scanner(System.in);
 //        inputAndPrintSecondSmallest(scanner);
 //        inputAndPrintMaxDifference(scanner);
+//        inputAndPrintLISLength(scanner);
+//        inputAndPrintSmallestUnreachableSum(scanner);
         scanner.close();
     }
 
@@ -66,6 +68,64 @@ public class Level3 {
 
         System.out.println("Hiệu lớn nhất là: " + maxDiff);
         System.out.println("(Được tính từ " + max + " - " + min + ")");
+    }
+
+//    Level 3, 3.3: Write a program that takes a list of integers as input and returns the longest increasing subsequence of the numbers
+    public static void inputAndPrintLISLength(Scanner scanner) {
+        System.out.print("Nhập số lượng: ");
+        int n = scanner.nextInt();
+
+        int[] arr = new int[n];
+        System.out.println("Nhập " + n + " số nguyên:");
+        for (int i = 0; i < n; i++) {
+            System.out.print("Nhập số thứ " + (i + 1) + ": ");
+            arr[i] = scanner.nextInt();
+        }
+
+        int[] dp = new int[n];
+        int[] prev = new int[n];
+        Arrays.fill(dp, 1);
+        Arrays.fill(prev, -1);
+
+        int maxLength = 1;
+        int endIndex = 0;
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j] && dp[i] < dp[j] + 1) {
+                    dp[i] = dp[j] + 1;
+                    prev[i] = j;
+                    if (dp[i] > maxLength) {
+                        maxLength = dp[i];
+                        endIndex = i;
+                    }
+                }
+            }
+        }
+
+        System.out.println("Độ dài của chuỗi con tăng dần dài nhất: " + maxLength);
+    }
+
+//    Level 3, 3.5: Write a program that takes a list of numbers as input and returns the smallest positive integer that cannot be represented as the sum of any subset of the list.
+    public static void inputAndPrintSmallestUnreachableSum(Scanner scanner) {
+        System.out.print("Nhập: ");
+        String[] parts = scanner.nextLine().split("\\s+");
+        int[] numbers = new int[parts.length];
+
+        for (int i = 0; i < parts.length; i++) {
+            numbers[i] = Integer.parseInt(parts[i]);
+        }
+
+        Arrays.sort(numbers);
+        int res = 1;
+        for (int num : numbers) {
+            if (num > res) {
+                break;
+            }
+            res += num;
+        }
+
+        System.out.println("result: " + res);
     }
 
 }
