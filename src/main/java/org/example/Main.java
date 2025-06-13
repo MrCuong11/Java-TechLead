02bbeb9f7c12c610911634b8eff0286af99c7e44
Cuongcuch <nmanhcuong.cpf@gmail.com>
2025-06-13 08:54:07 +0700

    Level 1, 1.1: Write a program that takes two numbers as inputs and displays their sum.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,45 +4,21 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Nhập số chuỗi: ");
-        int n = Integer.parseInt(scanner.nextLine());
-
-        List<String> strings = new ArrayList<>();
-        System.out.println("Nhập các chuỗi:");
-        for (int i = 0; i < n; i++) {
-            strings.add(scanner.nextLine());
-        }
-
-        // Integer.compare(x, y) trả về:
-        //
-        //-1 nếu x < y → x đứng trước
-        //
-        //1 nếu x > y → x đứng sau
-        strings.sort((a, b) -> {
-            int distinctA = countDistinctWords(a);
-            int distinctB = countDistinctWords(b);
-
-            if (distinctA != distinctB) {
-                return Integer.compare(distinctB, distinctA);
-            }
-
-            if (a.length() != b.length()) {
-                return Integer.compare(b.length(), a.length());
-            }
-
-            return a.compareTo(b);
-        });
-
-        System.out.println("Result:");
-        for (String s : strings) {
-            System.out.println(s);
-        }
+        inputAndSum(scanner);
+        scanner.close();
     }
 
-    public static int countDistinctWords(String s) {
-        String[] words = s.toLowerCase().split("\\s+");
-        Set<String> unique = new HashSet<>(Arrays.asList(words));
-        return unique.size();
+
+    // Level 1, 1.1: Write a program that takes two numbers as inputs and displays their sum.
+    public static void inputAndSum(Scanner scanner) {
+        System.out.print("Enter first number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Enter second number: ");
+        double num2 = scanner.nextDouble();
+
+        double sum = num1 + num2;
+
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
     }
 }
