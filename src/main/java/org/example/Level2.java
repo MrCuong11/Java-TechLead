4815d72c7964479ff2ca4c5ad425286c7e811d9a
Cuongcuch <nmanhcuong.cpf@gmail.com>
2025-06-15 14:52:40 +0700

    Fix 2.1

--- src/main/java/org/example/Level2.java
+++ src/main/java/org/example/Level2.java
@@ -18,31 +18,18 @@ public class Level2 {
     }
 
     //    Level 2, 2.1: Write a program that takes a list of numbers as input and returns the second largest number in the list
-    public static void inputAndFindSecondLargest(Scanner scanner) {
-        System.out.print("enter? ");
-        int count = scanner.nextInt();
-
-        int[] numbers = new int[count];
-
-        for (int i = 0; i < count; i++) {
-            System.out.print("Enter number " + (i + 1) + ": ");
-            numbers[i] = scanner.nextInt();
-        }
-
-        System.out.println("\nOriginal list: " + Arrays.toString(numbers));
-
+    public static Integer findSecondLargest(int[] numbers) {
         Arrays.sort(numbers);
+        int largest = numbers[numbers.length - 1];
 
-        int secondLargest = numbers[count - 1];
-        for (int i = count - 2; i >= 0; i--) {
-            if (numbers[i] < secondLargest) {
-                secondLargest = numbers[i];
-                break;
+        for (int i = numbers.length - 2; i >= 0; i--) {
+            if (numbers[i] < largest) {
+                return numbers[i];
             }
         }
-
-        System.out.println("The second largest number is: " + secondLargest);
+        return null;
     }
+
 
 
     //    Level 2, 2.2: Write a program that takes a list of strings as input and returns the longest word in the list.
